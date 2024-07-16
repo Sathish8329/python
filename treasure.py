6c5c08e3ed89a56c15e24513a1be6560a2371804
Sathish8329 <131761544+Sathish8329@users.noreply.github.com>
2024-07-16 11:54:13 +0530

    Update treasure.py

--- treasure.py
+++ treasure.py
@@ -1,13 +1,13 @@
 print("Welcome to Treasure Island\nNow you have to find the a big Island and you have to find the treasure in the Island.\n")
-direction = input("Do you want to go left or right?\n")
+direction = input("Do you want to go left or right?\n").lower()
 if direction == "right":
   print("Fall into a hole.\nGame Over.")
 else:
-  swim_wait = input("After turning left you reached the lake? Do you want to swim or wait for a boat?\n")
+  swim_wait = input("After turning left you reached the lake? Do you want to swim or wait for a boat?\n").lower()
   if swim_wait == "swim":
     print("Attacked by trout.\nGame Over.")
   else:
-    door = input("You have safely landed in Island, which has three doors, select any one to find the treasure eg red, blue, yellow?\n")
+    door = input("You have safely landed in Island, which has three doors, select any one to find the treasure eg red, blue, yellow?\n").lower()
     if door == "red":
       print("Burned by fire.\nGame Over")
     elif door == "blue":
